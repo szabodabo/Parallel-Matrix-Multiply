a080ba263ac842d3a784e08ce7681654d2a23202
Dakota <szabod@rpi.edu>
2012-03-11 22:27:25 -0400

    Screwed up.

--- mpimm.c
+++ mpimm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <rdtsc.h>
 #include <mtrand.h>
@@ -13,6 +14,7 @@ double CLOCK_RATE = 700000000.0; // change to 700000000.0 for Blue Gene/L
 int MY_RANK;
 
 int index_translate( int r, int c ) {
+//	printf("[%d] Translating r=%d, c=%d to %d\n", MY_RANK, r, c, (r * MATRIX_SIZE) + c);
 	return (r * MATRIX_SIZE) + c;
 }
 
@@ -91,22 +93,22 @@ int main(int argc, char **argv)
 	//  N operations... 
 	//Because of that, this rank will only store one row of C.
 
-/*	printf("[%d] Matrix A:\n", myRank);
+	printf("[%d] Matrix A:\n", myRank);
 	for (i = 0; i < partitionSize; i++) {
 		for (j = 0; j < matrix_size; j++) {
-			printf("%5.5lf ", A_ROWS[i][j]);
-		}
-		printf("\n");
-	}*/
-/*
-	printf("[%d] Matrix B (%d - %d):\n", myRank, col_offset, col_offset+partitionSize-1);
-	for (i = 0; i < matrix_size; i++) {
-		for (j = 0; j < partitionSize; j++) {
-			printf("%5.2lf ", B_COLS[i][j]);
+			printf("%5.5lf ", A_ROWS[index_translate(i, j)]);
 		}
 		printf("\n");
 	}
-*/
+
+	printf("[%d] Matrix B (%d - %d):\n", myRank, col_offset, col_offset+partitionSize-1);
+	for (i = 0; i < matrix_size; i++) {
+		for (j = 0; j < partitionSize; j++) {
+			printf("%5.2lf ", B_COLS[index_translate(i, j)]);
+		}
+		printf("\n");
+	}
+
 
 	while (partitionsRemaining > 0) {
 		printf("[%2d] %d / %d partitions remaining.\n", myRank, partitionsRemaining, commSize);
@@ -115,46 +117,47 @@ int main(int argc, char **argv)
 			startRecv = rdtsc();
 			int sourceRank = myRank -1;
 			if (sourceRank == -1) { sourceRank = commSize-1; }
-			MPI_Irecv(&recvBuffer, 1, MPI_Partition, sourceRank, MPI_ANY_TAG, MPI_COMM_WORLD, &requests[RECV]);
+			MPI_Irecv(recvBuffer, 1, MPI_Partition, sourceRank, MPI_ANY_TAG, MPI_COMM_WORLD, &requests[RECV]);
 			Bsource--;
 			if (Bsource == -1) { Bsource = commSize-1; }
 			col_offset = partitionSize * Bsource;
+			printf("[%d] Posted recv for BSrc %d\n", myRank, Bsource);
 
 			//Post a send to get the current B partition out of here
 			startSend = rdtsc();
 			int destRank = (myRank+1) % commSize;
-			MPI_Isend(&B_COLS, 1, MPI_Partition, destRank, 1, MPI_COMM_WORLD, &requests[SEND]);
-
-			int recvComplete = 0;
-			int sendComplete = 0;
-			int *completed_idxs = malloc(2 * sizeof(int));
-			int num_complete;
-			while (1) {
-				MPI_Testsome(2, requests, &num_complete, completed_idxs, MPI_STATUSES_IGNORE);
-				for (i = 0; i < num_complete; i++) {
-					if (completed_idxs[i] == SEND) {
-						sendComplete = 1;
-						finishSend = rdtsc();
-					} else if (completed_idxs[i] == RECV) {
-						recvComplete = 1;
-						finishRecv = rdtsc();
-					}
-				}
-				if (recvComplete == 1 && sendComplete == 1) {
-					break;
-				}
+			MPI_Isend(B_COLS, 1, MPI_Partition, destRank, 1, MPI_COMM_WORLD, &requests[SEND]);
+			printf("[%d] Posted send of current data\n", myRank);
+			int completedIdx;
+			for (i = 0; i < 2; i++) {
+				printf("[%d] Beginning wait...\n", myRank);
+				MPI_Waitany(2, requests, &completedIdx, MPI_STATUS_IGNORE);
+				if (completedIdx == RECV) {
+					finishRecv = rdtsc();
+					printf("[%d] Completed recv\n", myRank);
+				} else if (completedIdx == SEND) {
+					finishSend = rdtsc();
+					printf("[%d] Completed send\n", myRank);
+				} else { printf("[%d] PROBLEM!\n", myRank); }
+				requests[completedIdx] = MPI_REQUEST_NULL;
 			}
+			printf("[%d] Done with comm\n", myRank);
+			
 			irecv_bytes += (matrix_size * partitionSize * sizeof(double));
 			isend_bytes += (matrix_size * partitionSize * sizeof(double));
 			my_isend_time += (finishSend - startSend) / CLOCK_RATE;
 			my_irecv_time += (finishRecv - startRecv) / CLOCK_RATE;
 
+			printf("[%d] Moving buffer...\n", myRank);
+
+			memcpy(B_COLS, recvBuffer, matrix_size * partitionSize * sizeof(double));
+			printf("[%d] Done moving buffer\n", myRank);
 			//Move the next B from buffer to current
-			for (i = 0; i < matrix_size; i++) {
+		/*	for (i = 0; i < matrix_size; i++) {
 				for (j = 0; j < partitionSize; j++) {
 					B_COLS[index_translate(i, j)] = recvBuffer[index_translate(i, j)];
 				}
-			}
+			}*/
 		} //End if not first go
 
 /*		if (myRank == 0) {
@@ -168,6 +171,7 @@ int main(int argc, char **argv)
 		}*/
 
 		//Now compute a block of C for the current B partition
+		printf("[%d] Starting compute\n", myRank);
 		startComp = rdtsc();
 		for (i = 0; i < partitionSize; i++) {
 			for (j = 0; j < partitionSize; j++) {
@@ -176,6 +180,7 @@ int main(int argc, char **argv)
 			}
 		}
 		finishComp = rdtsc();
+		printf("[%d] Finished compute\n", myRank);
 		my_compute_time += (finishComp - startComp) / CLOCK_RATE;
 		partitionsRemaining--;
 	}
@@ -196,7 +201,9 @@ int main(int argc, char **argv)
 	}
 */
 
-	int PRINT_CORRECTNESS_CHECK = 0;
+	printf("[%d] Beginning stat analysis\n", myRank);
+
+	int PRINT_CORRECTNESS_CHECK = 1;
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	if (myRank == 0 && PRINT_CORRECTNESS_CHECK) {
@@ -214,22 +221,28 @@ int main(int argc, char **argv)
 
 	double my_isend_bw = (double) isend_bytes / my_isend_time;
 	double my_irecv_bw = (double) irecv_bytes / my_irecv_time;
-	double total_exec_time;
+	double total_exec_time = 0.0;
 	double max_isend_bw, min_isend_bw, sum_isend_bw, avg_isend_bw;
 	double max_irecv_bw, min_irecv_bw, sum_irecv_bw, avg_irecv_bw;
 
+	printf("[%d] Variables initialized.\n", myRank);
+	printf("[%d] MyTotalTime: %8.7lf sec\n", myRank, my_total_time);
+
 	//TODO: Not sure if max should be used here
-	MPI_Reduce(&my_total_time, &total_exec_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_total_time, &total_exec_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
-	MPI_Reduce(&my_isend_bw, &max_isend_bw, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&my_isend_bw, &min_isend_bw, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&my_isend_bw, &sum_isend_bw, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_isend_bw, &max_isend_bw, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_isend_bw, &min_isend_bw, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_isend_bw, &sum_isend_bw, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
-	MPI_Reduce(&my_irecv_bw, &max_irecv_bw, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&my_irecv_bw, &min_irecv_bw, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&my_irecv_bw, &sum_irecv_bw, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_irecv_bw, &max_irecv_bw, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_irecv_bw, &min_irecv_bw, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+	MPI_Allreduce(&my_irecv_bw, &sum_irecv_bw, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+	printf("[%d] Allreduces done!\n", myRank);
 
 	if (myRank == 0) { //Yay me
+		printf("Rank 0 printing stats!\n");
 		avg_isend_bw = sum_isend_bw / (double) commSize;
 		avg_irecv_bw = sum_irecv_bw / (double) commSize;
 
@@ -248,13 +261,27 @@ int main(int argc, char **argv)
 				min_isend_bw / 1000000.0, min_irecv_bw / 1000000.0, 
 				max_isend_bw / 1000000.0, max_irecv_bw / 1000000.0,
 				avg_isend_bw / 1000000.0, avg_irecv_bw / 1000000.0);
+		printf("[0] Closing data file...\n");
 		fclose(dataFile);
 	}
+	printf("[%d] Breakpoint!\n", myRank);
 
-//	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("[%d] Passed barrier\n", myRank); fflush(stdout);
 
 //	printf("[%d] SendWaits: %d; RecvWaits: %d\n", myRank, sendWait, recvWait);
 
-	MPI_Finalize();
+	printf("[%d] Freeing matrix buffers\n", myRank); fflush(stdout);
+//	free(A_ROWS);
+//	free(B_COLS);
+//	free(C_ROWS);
+	printf("[%d] Freeing receive buffer\n", myRank); fflush(stdout);
+//	free(recvBuffer);
+//	free(requests);
+	printf("[%d] Done freeing.\n", myRank); fflush(stdout);
+
+	int rc = MPI_Finalize();
+	printf("[%d] Return code of finalize: %d\n", myRank, rc); fflush(stdout);
 	return 0;
 }
